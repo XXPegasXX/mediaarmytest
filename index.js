5de0703ff5025675b3516f27a43032dbb997d82c
XXPegasXX <emivanov@inbox.ru>
2023-05-01 17:44:15 +0500

    Add some animations

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const menuBtn = document.getElementById("menu_button");
+const headerFull = document.getElementById("header_full_wrapper");
+const headerBtn = document.getElementById("header_button");
+const headerBtnSubstitute = document.getElementById("header_button_substitute");
+
+
 menuBtn.onclick = ()=>{
-    
+    headerFull.classList.toggle("header_full_wrapper_inactive");
+    menuBtn.classList.toggle("menu_button_cross");
+    headerBtn.classList.toggle("header_button_big");
+    headerBtnSubstitute.classList.toggle("header_button_substitute_active");
 };
