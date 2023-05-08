53a9b725cc410da80ce8407ca33e2497f67d8cac
XXPegasXX <emivanov@inbox.ru>
2023-05-08 18:35:35 +0500

    AddedValidationDraft

--- index.js
+++ index.js
@@ -5,6 +5,70 @@ const headerBtnSubstitute = document.getElementById("header_button_substitute");
 const appointmentPopup = document.getElementById("appointmentPopup");
 const appointmentPopupCloseBtn = document.getElementById("appointmentPopupCloseBtn");
 const appointmentFormWrapper = document.getElementById("appointmentFormWrapper");
+const appointmentFormName = document.getElementById("appointmentFormName");
+const appointmentFormNameError = document.getElementById("appointmentFormNameError");
+
+const appointmentFormPhone = document.getElementById("appointmentFormPhone");
+const appointmentFormPhoneError = document.getElementById("appointmentFormPhoneError");
+
+const appointmentFormVendor = document.getElementById("appointmentFormVendor");
+const appointmentFormVendorError = document.getElementById("appointmentFormVendorError");
+
+const appointmentFormDescription = document.getElementById("appointmentFormDescription");
+const appointmentFormDescriptionError = document.getElementById("appointmentFormDescriptionError");
+
+
+appointmentFormNameValidate = function () {
+    if (appointmentFormName.value =="") {
+        appointmentFormNameError.classList.add("validation_error_label_active");
+        return false;
+    } else {
+        appointmentFormNameError.classList.remove("validation_error_label_active");
+        return true;
+    }
+}
+appointmentFormName.onchange = appointmentFormNameValidate;
+
+appointmentFormPhone.onchange = function () {
+    if (appointmentFormPhone.value =="") {
+        appointmentFormPhoneError.classList.add("validation_error_label_active");
+    } else {
+        appointmentFormPhoneError.classList.remove("validation_error_label_active");
+    }
+}
+
+appointmentFormVendor.onchange = function () {
+    if (appointmentFormVendor.value =="") {
+        appointmentFormVendorError.classList.add("validation_error_label_active");
+    } else {
+        appointmentFormVendorError.classList.remove("validation_error_label_active");
+    }
+}
+
+appointmentFormDescription.onchange = function () {
+    if (appointmentFormDescription.value =="") {
+        appointmentFormDescriptionError.classList.add("validation_error_label_active");
+    } else {
+        appointmentFormDescriptionError.classList.remove("validation_error_label_active");
+    }
+}
+
+// someBtn.onclick = function() {
+//     var nameIsValid = appointmentFormNameValidate();
+
+//     if (nameIsValid &&
+//         phoneIsValid &&
+//         vendorIsValid &&
+//         textIsValid) {
+//             console.log("Send data")
+//         }
+// }
+
+
+
+
+
+
 
 
 menuBtn.onclick = ()=>{
