3a7dd9f3882c6be2179fb90e3e2367bf02b0252c
XXPegasXX <emivanov@inbox.ru>
2023-05-08 16:23:15 +0500

    Popup added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const menuBtn = document.getElementById("menu_button");
 const headerFull = document.getElementById("header_full_wrapper");
 const headerBtn = document.getElementById("header_button");
 const headerBtnSubstitute = document.getElementById("header_button_substitute");
+const appointmentPopup = document.getElementById("appointmentPopup");
+const appointmentPopupCloseBtn = document.getElementById("appointmentPopupCloseBtn");
+const appointmentFormWrapper = document.getElementById("appointmentFormWrapper");
 
 
 menuBtn.onclick = ()=>{
@@ -10,3 +13,14 @@ menuBtn.onclick = ()=>{
     headerBtn.classList.toggle("header_button_big");
     headerBtnSubstitute.classList.toggle("header_button_substitute_active");
 };
+
+const togglePopup = function() {
+    appointmentPopup.classList.toggle("popup_active");
+    appointmentFormWrapper.classList.toggle("appointment_form_wrapper_active");
+}
+
+headerBtn.onclick = togglePopup;
+
+appointmentPopupCloseBtn.onclick = togglePopup;
+
+
